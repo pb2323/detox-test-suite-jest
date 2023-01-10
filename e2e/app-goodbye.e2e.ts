4ab501560b80d1d976bf6869a381e73045b05722
Puneet Bajaj <pbajaj0023@gmail.com>
2023-01-10 17:23:48 +0530

    initial commit

---
+++ e2e/app-goodbye.e2e.ts
@@ -0,0 +1,12 @@
+import { device, expect, element, by } from 'detox';
+
+describe('Example (goodbye)', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show goodbye screen after tap', async () => {
+    await element(by.id('goodbye_button')).tap();
+    await expect(element(by.text('Goodbye, World!!!'))).toBeVisible();
+  });
+});
